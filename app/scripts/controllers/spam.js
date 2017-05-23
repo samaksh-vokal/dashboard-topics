d310ce44a1373b27fd610f8dd677f7c7b24a17b1
Yatender <yatender@Yatenders-iMac-Pro.local>
2017-05-23 09:18:41 +0530

    fixing for uglify

--- app/scripts/controllers/spam.js
+++ app/scripts/controllers/spam.js
@@ -4,7 +4,6 @@ angular.module('okTalkApp')
             $('[data-toggle="tooltip"]').tooltip();
         });
 
-        // $scope.reports = [{"title":"Asdfghjjlljgf","reporters":[{"user_id":765,"report":1,"phone":"8217306529","name":"ராஜா","handle":"ikik"}],"profile_image":"https://s3-ap-southeast-1.amazonaws.com/ok.talk.stagingbucket/user_742/ab0ded71-cd16-463f-aac1-2584d5ebf544_voke_img_crop_f85216b9-46ed-4e09-a473-8fa83f090c29.jpg","payload":"https://s3-ap-southeast-1.amazonaws.com/oktalk.video/630f601e-3a0f-11e7-b89e-066de2de0677.mp3","name":"Aman 😇😁","lang":"ta","img":null,"handle":"aman.arora879t","created_at":"2017-05-17T08:54:59Z","count":1,"content_id":"630f601e-3a0f-11e7-b89e-066de2de0677"},{"title":"Dkxjxkckccjxixixidud","reporters":[{"user_id":537,"report":1,"phone":"7411125418","name":"samsung hefhhg fhh","handle":"samsung"}],"profile_image":"https://s3-ap-southeast-1.amazonaws.com/ok.talk.stagingbucket/user_991/aad6f004-b9fd-431c-8335-32c02e1f354e_voke_img_crop_2f18b373-5d16-4cab-871b-da0b0dbd44ea.jpg","payload":"https://s3-ap-southeast-1.amazonaws.com/oktalk.video/9ec0ad94-3617-11e7-ab8c-066de2de0677.mp3","name":"vyshak5","lang":"kn","img":"https://s3-ap-southeast-1.amazonaws.com/ok.talk.stagechannels/sim1/voke_img_crop_f4fa3b60-67bc-4bc9-8fd4-77d59b634354.jpg","handle":"vyshak5","created_at":"2017-05-17T09:45:11Z","count":1,"content_id":"9ec0ad94-3617-11e7-ab8c-066de2de0677"},{"title":"Gbbbjgdssdfgg","reporters":[{"user_id":742,"report":1,"phone":"7795023069","name":"Aman 😇😁","handle":"aman.arora879t"}],"profile_image":"","payload":"https://s3-ap-southeast-1.amazonaws.com/ok.talk.stagechannels/shweta.gqebbdk/c5c4ab8d-4af0-481c-bf4b-123a334fcc4d1493924848602.mp3","name":"testhkmznx","lang":"hi","img":"https://s3-ap-southeast-1.amazonaws.com/ok.talk.stagechannels/aman.arora879t/voke_img_crop_454ed690-b910-46ca-abc5-7e171a2cd468.jpg","handle":"shweta.gqebbdk","created_at":"2017-05-10T12:50:07Z","count":1,"content_id":"ea3631ec-30fc-11e7-89b4-066de2de0677"},{"title":"Vkbivibobobob","reporters":[{"user_id":742,"report":2,"phone":"7795023069","name":"Aman 😇😁","handle":"aman.arora879t"}],"profile_image":"","payload":"https://s3-ap-southeast-1.amazonaws.com/oktalk.video/51a47b14-354d-11e7-9ce4-066de2de0677.mp3","name":"Aniruddha","lang":"hi","img":"https://s3-ap-southeast-1.amazonaws.com/ok.talk.stagechannels/vokalani/voke_img_crop_51be5042-fb38-4c15-b736-0db274a4eeea.jpg","handle":"vokalani","created_at":"2017-05-10T12:55:53Z","count":1,"content_id":"51a47b14-354d-11e7-9ce4-066de2de0677"},{"title":"Dvhfhdhfufjgfyuig","reporters":[{"user_id":742,"report":2,"phone":"7795023069","name":"Aman 😇😁","handle":"aman.arora879t"}],"profile_image":"","payload":"https://s3-ap-southeast-1.amazonaws.com/oktalk.video/7d439cb4-34ad-11e7-804f-066de2de0677.mp3","name":"vyshak3","lang":"hi","img":"https://s3-ap-southeast-1.amazonaws.com/ok.talk.stagechannels/dhvb/voke_img_crop_0d2aad10-cb68-4420-a39a-6f24ae179bb0.jpg","handle":"vyshak3","created_at":"2017-05-10T13:00:04Z","count":1,"content_id":"7d439cb4-34ad-11e7-804f-066de2de0677"},{"title":"Gbbbjgdssdfgg","reporters":[{"user_id":742,"report":2,"phone":"7795023069","name":"Aman 😇😁","handle":"aman.arora879t"}],"profile_image":"","payload":"https://s3-ap-southeast-1.amazonaws.com/ok.talk.stagechannels/shweta.gqebbdk/31284ff7-50d2-4dcd-9ecb-6151e84e7d201493925149059.mp3","name":"testhkmznx","lang":"hi","img":"https://s3-ap-southeast-1.amazonaws.com/ok.talk.stagechannels/aman.arora879t/voke_img_crop_454ed690-b910-46ca-abc5-7e171a2cd468.jpg","handle":"shweta.gqebbdk","created_at":"2017-05-10T12:50:00Z","count":1,"content_id":"9dc57ca4-30fd-11e7-9310-066de2de0677"},{"title":"Qqqqqqqqqwwww","reporters":[{"user_id":742,"report":2,"phone":"7795023069","name":"Aman 😇😁","handle":"aman.arora879t"}],"profile_image":"","payload":"https://s3-ap-southeast-1.amazonaws.com/oktalk.video/a661cdb4-34ad-11e7-a713-066de2de0677.mp3","name":"vyshak3","lang":"hi","img":"https://s3-ap-southeast-1.amazonaws.com/ok.talk.stagechannels/shweta.gqebbdk/voke_img_crop_1491c207-fd79-414a-b993-3f8217da60be.jpg","handle":"vyshak3","created_at":"2017-05-10T12:59:18Z","count":1,"content_id":"a661cdb4-34ad-11e7-a713-066de2de0677"}];
 
         var objR = function(content_id, owner_id) {
             this.content_id = content_id;
@@ -18,7 +17,7 @@ angular.module('okTalkApp')
         }
 
 
-        $scope.getTotal = (reporters) => {
+        $scope.getTotal = function(reporters) {
             var reportDetails = {
                 spam: 0,
                 abuse: 0,
@@ -38,7 +37,7 @@ angular.module('okTalkApp')
             return 'spam:(' + reportDetails.spam + ')   abuse:(' + reportDetails.abuse + ') others:(' + reportDetails.others + ')';
         };
 
-        $scope.doAction = (content_id, action, owner_id) => {
+        $scope.doAction = function(content_id, action, owner_id) {
             console.log(content_id, action);
             var k = new objR2(content_id, owner_id,"");
             console.log(k);
@@ -48,7 +47,7 @@ angular.module('okTalkApp')
             });
         }
 
-        $scope.doUndo = (item) => {
+        $scope.doUndo = function(item) {
             console.log(item);
             var status =200+ item.report%10;
             var k = new objR2(item.content_id, item.owner_id,status);
@@ -59,7 +58,7 @@ angular.module('okTalkApp')
             });
         }
 
-          $scope.doActionFromIgnored = (item,type) => {
+          $scope.doActionFromIgnored = function(item,type) {
             console.log(item);
             var status = item.report%10;
             if(type === "spam_undo"){
@@ -133,7 +132,7 @@ angular.module('okTalkApp')
         $scope.popup1 = {
             opened: false
         };
-        $scope.getPeople = (reporters) => {
+        $scope.getPeople = function(reporters) {
             var x = [];
             var k = '';
             reporters.forEach(function (element) {
@@ -144,7 +143,7 @@ angular.module('okTalkApp')
             return x;
         };
 
-        $scope.getData = () => {
+        $scope.getData = function() {
             if ($scope.type === "reported") {
                 setTimeout(function () {
                     $scope.show(1);
@@ -161,7 +160,7 @@ angular.module('okTalkApp')
                 }, 1500);
 
             }
-            $scope.url = 'http://int.okapi.com/v1/users/spam/' + $scope.type + '?lang=' + $scope.lang + '&from=' + formatDate($scope.channel.dt) + '&to=' + formatDate($scope.channel.dt2);
+            $scope.url = 'http://int.oktalk.com/v1/users/spam/' + $scope.type + '?lang=' + $scope.lang + '&from=' + formatDate($scope.channel.dt) + '&to=' + formatDate($scope.channel.dt2);
             apiFactory.doGetCall($scope.url)
                 .then(function (response) {
                     console.log(response.data);
@@ -173,12 +172,12 @@ angular.module('okTalkApp')
                 });
         }
 
-        $scope.getName = (num) => {
+        $scope.getName = function(num) {
             var arr = ['-', 'Spam', 'Abusive', 'Others']
             return arr[num] || '';
         };
 
-        $scope.show = (item) => {
+        $scope.show = function(item) {
             if (item === 1) {
                 $('.action2').hide();
                 $('.action3').hide();
